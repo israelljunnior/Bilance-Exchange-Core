9fa5780df96f6da72031f20de9d901b46cc2fe76
Israel Junior <israelljunnior15987426@hotmail.com>
2020-09-30 19:51:02 -0300

    Enums for UserType

---
+++ Bilance Exchange.Domain/Enum/UserTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bilance_Exchange.Domain.Enum
+{
+    enum UserTypeEnum
+    {
+        admin = 1,
+        investor = 2,
+        developer=3
+    }
+}
