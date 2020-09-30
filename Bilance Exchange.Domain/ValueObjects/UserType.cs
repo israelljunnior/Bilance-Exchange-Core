320ba2a63ab9aef4643c34d1331b5b6a9df3e950
Israel Junior <israelljunnior15987426@hotmail.com>
2020-09-30 19:52:34 -0300

    Adding Value Objects

---
+++ Bilance Exchange.Domain/ValueObjects/UserType.cs
@@ -0,0 +1,13 @@
+﻿using Bilance_Exchange.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bilance_Exchange.Domain.ValueObjects
+{
+    public class UserType : ValueObject<UserType>
+    {
+       private int Id { get; set; }
+       private UserTypeEnum userType { get; set; }
+    }
+}
---
+++ Bilance Exchange.Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bilance_Exchange.Domain.ValueObjects
+{
+    public abstract class ValueObject<TEntity> where TEntity : ValueObject<TEntity>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return EqualsCore(valueObject);
+        }
+    }
+}
