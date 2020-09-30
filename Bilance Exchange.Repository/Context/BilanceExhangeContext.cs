ba61329893991e2090f2dc1b26ede66ff0f13aed
Israel Junior <israelljunnior15987426@hotmail.com>
2020-09-30 20:05:57 -0300

    Adding basic repository

---
+++ Bilance Exchange.Repository/Context/BilanceExhangeContext.cs
@@ -0,0 +1,18 @@
+﻿using Bilance_Exchange.Domain.Entity;
+using Bilance_Exchange.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilanceExchange.Repository.Context
+{
+    class BilanceExhangeContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CoinBalance> CoinBalances { get; set; }
+        public DbSet<UserType> UserType { get; set; }
+    }
+}
