ff834269950973dadf1cb58e920fddc7c4cdfabb
Israel Junior <israelljunnior15987426@hotmail.com>
2020-10-09 19:35:36 -0300

    User Validator and Filter Validation

---
+++ BilanceExchange.Domain/Validators/UserValidator.cs
@@ -0,0 +1,26 @@
+﻿using Bilance_Exchange.Domain.Entity;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bilance_Exchange.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Name)
+                .NotEmpty();
+            RuleFor(user => user.Email)
+                .NotEmpty()
+                .EmailAddress(EmailValidationMode.Net4xRegex);
+            RuleFor(user => user.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+            RuleFor(user => user.UserTypeId)
+                .NotNull();
+        }
+    }
+}
---
+++ BilanceExchange.Domain/Validators/ValidationFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Web.Http.Filters;
+
+namespace Bilance_Exchange.Domain.Validators
+{
+    public class ValidateModelAttribute : Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute
+    {
+        public  void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
