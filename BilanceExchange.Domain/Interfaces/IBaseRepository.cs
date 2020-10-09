636b7039ff5b275d4239ebc2e62c0ae7b62017cf
Israel Junior <israelljunnior15987426@hotmail.com>
2020-10-09 20:27:39 -0300

    Save Function on BaseRepository

--- BilanceExchange.Domain/Interfaces/IBaseRepository.cs
+++ BilanceExchange.Domain/Interfaces/IBaseRepository.cs
@@ -6,6 +6,7 @@ namespace Bilance_Exchange.Domain.Interfaces
 {
     public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
     {
+        void Save(TEntity entity);
         TEntity findByID(int id);
         IEnumerable<TEntity> findAll();
         void remove(int id);
