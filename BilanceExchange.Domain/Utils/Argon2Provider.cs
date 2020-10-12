554bc6c0be786171dcd910ec9e6af3309828a841
Israel Junior <israelljunnior15987426@hotmail.com>
2020-10-11 23:30:03 -0300

    Argon2 Provider

---
+++ BilanceExchange.Domain/Utils/Argon2Provider.cs
@@ -0,0 +1,36 @@
+﻿using Konscious.Security.Cryptography;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Bilance_Exchange.Domain.Services.Security
+{
+    public class Argon2Provider
+    {
+
+        public  byte[] CreateSalt(int ByteSize)
+        {
+            var buffer = new byte[ByteSize];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+
+        public byte[] HashPassword(string password, byte[] salt)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+            argon2.Salt = salt;
+            argon2.DegreeOfParallelism = 8; // four cores
+            argon2.Iterations = 4;
+            argon2.MemorySize = 1024 * 1024; // 1 GB
+
+            return argon2.GetBytes(16);
+        }
+        public bool VerifyHash(string password, byte[] salt, byte[] hash)
+        {
+            var newHash = HashPassword(password, salt);
+            return hash.SequenceEqual(newHash);
+        }
+    }
+}
